e8bca5408364c68e7f3aafde98e84b5308f25f89
Daniil Vinogradov <xitrix@bk.ru>
2022-07-03 19:13:22 +0300

    Initial Commit

---
+++ NXKit/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  NXKit
+//
+//  Created by Даниил Виноградов on 03.07.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
